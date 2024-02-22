5c530a70a2625535b4b909347d0a93fa5356018f
Yahir Alberto <136031489+yahirdev13@users.noreply.github.com>
2024-02-22 10:53:24 -0600

    primer commit del proyecto

---
+++ src/main/java/mx/com/utez/projectmoviesbackend/ProjectMoviesBackendApplication.java
@@ -0,0 +1,13 @@
+package mx.com.utez.projectmoviesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMoviesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMoviesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/com/utez/projectmoviesbackend/ProjectMoviesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.utez.projectmoviesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMoviesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
